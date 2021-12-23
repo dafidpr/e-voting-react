eac57c1f4a3f07ea082dd8a38112d676ef947800
David Mahbubi <ulrichdavid0370@gmail.com>
2021-12-24 01:38:59 +0700

    Removed fixed-bottom in Footer.jsx

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="mt-5 bg-white p-4 fixed-bottom">
+        <footer className="mt-5 bg-white p-4">
             <div class="footer-copyright text-center py-3">
                 Developed by <i className="fa fa-heart text-danger"></i>
                 <strong><a href="https://webmediadigital.com" target="_blank"> CV. Web Media Solusi Digital</a></strong> | © 2021 Copyright
